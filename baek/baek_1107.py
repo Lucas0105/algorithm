ede0446f3b148fadba8efeabadbf0bcb9ed4aa44
Lucas0105 <dnjswo105@gmail.com>
2022-07-25 22:57:40 +0900

    백준 1107번 brute force 성공

--- baek/baek_1107.py
+++ baek/baek_1107.py
@@ -1,20 +1,37 @@
-N = int(input())
 
+def remocorn(result, check_btn, N):
+    global min_num
+
+    for num in check_btn:
+        result = result + str(num)
+
+        result_num = abs(int(result)-N) + len(result)
+
+        if min_num > result_num :
+            min_num = result_num
+
+        if len(result) < len(str(N)) + 1:
+            remocorn(result, check_btn, N)
+
+        result = result[:-1]
+
+N = int(input())
+check = list(range(10))
 err_btn_cnt = int(input())
 
-err_btn = list(map(int, input().split()))
+err_btn = set()
 
-current_num = 100
+if err_btn_cnt:
+    err_btn = list(map(int, input().split()))
 
-result = 0
+check_btn = set(check) - set(err_btn)
 
-N_list = list(N)
+min_num = abs(N-100)
 
-for char in N_list:
-    min = 20
-    for i in range(10):
-        if i not in err_btn : 
+if err_btn_cnt == 0 :
+    min_num = min(min_num, len(str(N)))
+else :
+    remocorn('', check_btn, N)
+    
 
-
-
-print(result)
+print(min_num)
