fe9257c55a8d1e47cfc0fb8ab39a5f9273538118
Lucas0105 <dnjswo105@gmail.com>
2024-12-20 21:40:02 +0900

    algo: 백준 G1 17143

--- java/src/baekjoon/Class1G17143.java
+++ java/src/baekjoon/Class1G17143.java
@@ -7,17 +7,25 @@ import java.io.InputStreamReader;
 public class Class1G17143 {
     static int R, C, M, res;
     static Shark[][] sharkTable = new Shark[101][101];
+    static int[] dr = new int[] {-1, 0, 1, 0};
+    static int[] dc = new int[] {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] a = br.readLine().split(" ");
-        R = Integer.parseInt(a[0]);C = Integer.parseInt(a[0]);M = Integer.parseInt(a[0]);
+        R = Integer.parseInt(a[0]);C = Integer.parseInt(a[1]);M = Integer.parseInt(a[2]);
 
         for(int i = 0; i < M; i++) {
             String[] b = br.readLine().split(" ");
             int r = Integer.parseInt(b[0]);int c = Integer.parseInt(b[1]);
             int s = Integer.parseInt(b[2]);int d = Integer.parseInt(b[3]);
             int z = Integer.parseInt(b[4]);
-            Shark newShark = new Shark(s, d, z);
+            d-=1;
+            if (d == 1)
+                d = 2;
+            else if(d == 2)
+                d = 1;
+
+            Shark newShark = new Shark(r, c, s, d, z);
             sharkTable[r][c] = newShark;
         }
 
@@ -35,6 +43,7 @@ public class Class1G17143 {
             if (sharkTable[row][pos] != null ){
                 int w = sharkTable[row][pos].z;
                 sharkTable[row][pos] = null;
+                M -= 1;
                 return w;
             }
         }
@@ -42,14 +51,68 @@ public class Class1G17143 {
     }
 
     public static void moveShark() {
+        Shark[] sharks = new Shark[M];
+        int idx = 0;
+        for(int row = 1; row <= R; row++){
+            for(int col = 1; col <=C; col++){
+                if(sharkTable[row][col] != null){
+                    sharks[idx] = sharkTable[row][col];
+                    idx +=1 ;
+                }
+            }
+        }
+        sharkTable = new Shark[R+1][C+1];
 
+        int removeShark = 0;
+        for(int i = 0; i < M; i++) {
+            int moveR = sharks[i].s % ((R-1) * 2);
+            int moveC = sharks[i].s % ((C-1) * 2);
+
+            for(int j = 1; j <= moveR; j++){
+                if(dr[sharks[i].d] == 0)
+                    break;
+                sharks[i].r += dr[sharks[i].d];
+
+                if(sharks[i].r == 0){
+                    sharks[i].r = 2;
+                    sharks[i].d = (sharks[i].d + 2) % 4;
+                } else if(sharks[i].r == R + 1){
+                    sharks[i].r = R-1;
+                    sharks[i].d = (sharks[i].d + 2) % 4;
+                }
+            }
+
+            for(int j = 1; j <= moveC; j++){
+                if(dc[sharks[i].d] == 0)
+                    break;
+                sharks[i].c += dc[sharks[i].d];
+                if(sharks[i].c == 0 ){
+                    sharks[i].c = 2;
+                    sharks[i].d = (sharks[i].d + 2) % 4;
+                } else if(sharks[i].c == C + 1) {
+                    sharks[i].c = C -1;
+                    sharks[i].d = (sharks[i].d + 2) % 4;
+                }
+
+            }
+
+            if(sharkTable[sharks[i].r][sharks[i].c] != null){
+                if(sharks[i].z > sharkTable[sharks[i].r][sharks[i].c].z){
+                    sharkTable[sharks[i].r][sharks[i].c] = sharks[i];
+                }
+                removeShark += 1;
+            } else {
+                sharkTable[sharks[i].r][sharks[i].c] = sharks[i];
+            }
+        }
+        M-= removeShark;
     }
     
     public static class Shark{
-        int s, d, z;
+        int r, c, s, d, z;
 
-        public Shark(int s, int d, int z){
-            this.s = s;this.d = d;this.z = z;
+        public Shark(int r, int c, int s, int d, int z){
+            this.r = r;this.c = c;this.s = s;this.d = d;this.z = z;
         }
     }
 }
