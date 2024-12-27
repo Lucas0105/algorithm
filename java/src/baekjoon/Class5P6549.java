1351e80b5c8951132315b05c4e135dda4ee67cd4
Lucas0105 <dnjswo105@gmail.com>
2024-12-27 22:08:46 +0900

    algo: 백준 플래5

--- java/src/baekjoon/Class5P6549.java
+++ java/src/baekjoon/Class5P6549.java
@@ -3,11 +3,12 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Class5P6549 {
     static int N;
-    static int[] bars;
+    static long[] bars;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -19,12 +20,34 @@ public class Class5P6549 {
             if(N == 0)
                 break;
 
-            bars = new int[N];
+            bars = new long[N + 1];
             for(int i = 0; i < N; i++) {
-                bars[i] = Integer.parseInt(st.nextToken());
+                bars[i] = Long.parseLong(st.nextToken());
             }
+            bars[N] = -1;
 
-            
+            long res = searchMaxBar();
+            System.out.println(res);
         }
     }
+
+    public static long searchMaxBar() {
+        Stack<Integer> stack = new Stack<>();
+        long maxValue = 0;
+
+        for(int i = 0; i <= N; i++) {
+
+            while(!stack.isEmpty() && bars[stack.peek()] >= bars[i]){
+                long height = bars[stack.pop()];
+
+                int width = stack.isEmpty() ? i : i - 1 - stack.peek();
+                maxValue = Math.max(maxValue, height * width);
+            }
+
+            stack.push(i);
+        }
+
+        return maxValue;
+    }
+
 }
