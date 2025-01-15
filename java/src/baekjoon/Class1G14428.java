7b67d3668ba96c8722424c3396ac086794ef7c72
Lucas0105 <dnjswo105@gmail.com>
2025-01-15 21:29:09 +0900

    algo: 백준 골드1

--- java/src/baekjoon/Class1G14428.java
+++ java/src/baekjoon/Class1G14428.java
@@ -12,21 +12,15 @@ public class Class1G14428 {
     static int N, M;
     static int[] arr;
     static int[] tree;
-    static ArrayList<Integer>[] memory = new ArrayList[100001];
-    static HashMap<Integer, Integer> hMap = new HashMap<>();
-    static int hIdx = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N + 1];
-        for(int i = 0; i <= 100000; i++){
-            memory[i] = new ArrayList<>();
-        }
 
         StringTokenizer st= new StringTokenizer(br.readLine());
+        arr[0] = (int)2e10;
         for(int i = 1; i <= N; i++){
             arr[i]  = Integer.parseInt(st.nextToken());
-            memory[getIdx(arr[i])].add(i);
         }
         int h = (int)Math.ceil(Math.log(N) / Math.log(2)) + 1;
         h = (int)Math.pow(2, h);
@@ -43,21 +37,10 @@ public class Class1G14428 {
             int c = Integer.parseInt(st.nextToken());
 
             if(a == 1){
-                memory[getIdx(arr[b])].removeIf(s-> s == b);
-                memory[getIdx(c)].add(b);
                 arr[b] = c;
-                update(1, N, 1, b, c);
+                update(1, N, 1, b);
             } else {
-                int value = query(1, N, 1, b, c);
-                int res = (int)1e10;
-                for(int j = 0; j < memory[getIdx(value)].size(); j++){
-                    int tmp = memory[getIdx(value)].get(j);
-                    if(tmp < b || tmp > c)
-                        continue;
-                    res = Math.min(res, memory[getIdx(value)].get(j));
-                }
-
-                System.out.println(res);
+                System.out.println(query(1, N, 1, b, c));
             }
 
         }
@@ -65,36 +48,36 @@ public class Class1G14428 {
 
     public static void init(int start, int end, int node) {
         if (start == end) {
-            tree[node] = arr[start];
+            tree[node] = start;
             return;
         }
         int mid = (start + end) / 2;
         init(start, mid, node * 2);
         init(mid + 1, end, node * 2 + 1);
 
-        tree[node] = Math.min(tree[node * 2], tree[node * 2 + 1]);
+        tree[node] = getMinIdx(tree[node * 2], tree[node * 2 + 1]);
     }
 
 
-    public static void update(int start, int end, int node, int idx, int diff){
+    public static void update(int start, int end, int node, int idx){
         if(start == end){
-            tree[node] = diff;
+            tree[node] = idx;
             return;
         }
 
         int mid = (start + end) / 2;
 
         if(idx <= mid)
-            update(start, mid, node * 2, idx, diff);
+            update(start, mid, node * 2, idx);
         else
-            update(mid + 1, end, node * 2 + 1, idx, diff);
+            update(mid + 1, end, node * 2 + 1, idx);
 
-        tree[node] = Math.min(tree[node * 2], tree[node * 2 + 1]);
+        tree[node] = getMinIdx(tree[node * 2], tree[node * 2 + 1]);
     }
 
     public static int query(int start, int end, int node, int left, int right) {
         if (start > right || end < left) {
-            return (int) 2e10;
+            return 0;
         }
 
         if (start >= left && end <= right) {
@@ -105,15 +88,16 @@ public class Class1G14428 {
         int leftV = query(start, mid, node * 2, left, right);
         int rightV = query(mid + 1, end, node * 2 + 1, left, right);
 
-        return Math.min(leftV, rightV);
+        return getMinIdx(leftV, rightV);
     }
 
-    public static int getIdx(int v){
-        if(hMap.containsKey(v))
-            return hMap.get(v);
-        else{
-            hMap.put(v, hIdx);
-            return hIdx++;
+    public static int getMinIdx(int n1, int n2) {
+        if(arr[n1] < arr[n2]){
+            return n1;
+        } else if(arr[n1] == arr[n2]){
+            return Math.min(n1, n2);
+        }else {
+            return n2;
         }
     }
 }
