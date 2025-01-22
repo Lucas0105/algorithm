c95eae9e03f32a733c80abd9d0392c9851cae540
Lucas0105 <dnjswo105@gmail.com>
2025-01-22 22:40:08 +0900

    algo: softeer rank 1

---
+++ java/src/softeer/나무출력.java
@@ -0,0 +1,27 @@
+package softeer;
+
+import java.io.IOException;
+
+public class 나무출력 {
+    public static void main(String args[]) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("           *********           \n");
+        sb.append("      *****         ****       \n");
+        sb.append("    **                  **     \n");
+        sb.append("   *                      *    \n");
+        sb.append(" **                        **  \n");
+        sb.append("*                            * \n");
+        sb.append("*                            * \n");
+        sb.append("**                          ** \n");
+        sb.append(" **  *    ****  ****    *  **  \n");
+        sb.append("   ** ***** ****** ***** **    \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("            ******             \n");
+        sb.append("           ********");
+
+        System.out.println(sb);
+    }
+}
